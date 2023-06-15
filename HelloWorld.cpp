e3374d2156f70267680f59919a8d04f5275903bd
abdullah <3703abdullahashraf@gmail.com>
2023-06-15 14:10:10 +0530

    Lecture1

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>  //This line means Header
+
+using namespace std;  // std makes sure that cout is a part of "iostream"
+
+int main()   //we have written a main "function"
+{
+   cout<<"Hello World";
+
+   return 0;
+}
