8ad90570bbe56e63f89399775aa2dcdc72b23ce1
abdullah <3703abdullahashraf@gmail.com>
2023-06-20 15:10:04 +0530

    Lecture 4 part-1

---
+++ conditional.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int c,d;
+
+    cout<<"Input 4 integers a,b,c and d"<<endl;
+
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+    //statement : a>b  c>d
+
+
+    cout<<(a>b || c>d);
+
+    //C++ interprets true as 1
+    //C++ interprets false as 0
+
+}
+
+
+
+// Execute AND and OR statements and print out the boolean :
+
+
+
+
+// Execute if else statement  :
+
+
+
+
+
+// Switch case conditional statement :
+
