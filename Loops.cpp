e3b2f9f2503067115921807993ed32f391f65044
abdullah <3703abdullahashraf@gmail.com>
2023-06-25 02:41:48 +0530

    Lecture-6, Part-2

---
+++ Loops.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   /* int i,j,k;
+    i=0;
+    j=0;
+    k=0;
+
+    cout<<i<<" "<<j<<" "<<k<<endl; 
+    j = i++;
+    cout<<i<<" "<<j<<" "<<k<<endl; 
+    k = ++i;
+    cout<<i<<" "<<j<<" "<<k<<endl;*/
+
+    int i;
+
+    for(i=1;i<101;i++ /* called the update which happens after the iterations(part of a loop is completed)*/) 
+    {
+        cout<<i<<" ";
+    } 
+
+}
