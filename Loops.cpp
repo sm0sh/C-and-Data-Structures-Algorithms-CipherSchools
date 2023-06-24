d5fd037497e23a6b889385ce8d24ffcdbeec3ecb
abdullah <3703abdullahashraf@gmail.com>
2023-06-25 03:04:37 +0530

    Lecture-6, Part-3

--- Loops.cpp
+++ Loops.cpp
@@ -14,11 +14,36 @@ int main()
     k = ++i;
     cout<<i<<" "<<j<<" "<<k<<endl;*/
 
-    int i;
+    //int i;
 
-    for(i=1;i<101;i++ /* called the update which happens after the iterations(part of a loop is completed)*/) 
+    /*for(i=1;i<101;i++)//called the update which happens after the iterations(part of a loop is completed)) 
     {
         cout<<i<<" ";
-    } 
+    }*/
+
+    //i=1;       //initialization
+    //while(i<101)   //condition
+    //{
+    //    cout<<i<<" ";    //loop
+    //    i++;             //update
+    //}
+
+    int i;
+    int input;
+
+
+    // guess the number 65
+    // the user has 100 chances
+    i=100;
+
+    for(i=1;i<101;i++)
+    {
+        cin>>input;
+        if(input == 65)  // only takes place if the input is exactly 65
+        {
+            cout<<"congrats you have guessed correct ";
+            break;
+        }
+    }
 
 }
