b9ef67c51a5ca6090fd43213c8cdf51bfa9eb55a
abdullah <3703abdullahashraf@gmail.com>
2023-07-04 15:29:17 +0530

    Lecture-9, Part-2

--- morefunctions.cpp
+++ morefunctions.cpp
@@ -1,44 +1,42 @@
-/*int compare(int a,int b)       // compares 2 integers and returns the greater one
-{
-    /*if(a>b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }*/
-
-    /*if(a==5)
-    {
-        return 1;
-    }
-    return 0;
-}*/
-
 #include<iostream>
 using namespace std;
 
-int sum(int a,int b)
-{
-    int c;
+int sum(int a,int b); // I have declared that a function called sum exists but the body is not formed yet
 
+/*{
+    int c;
     c = a+b;
+
+
+    a = 10;
+    b = 11;
    
+
     return c;
-}
+}*/
 
 
 int main()
 {
     int a,b,c;
+    a=3;
+    b=5;
 
+    cout<<a<<" "<<b<<endl;   // these values are before I call the function "sum"
+ 
     a=3;
     b=5;
 
     c = sum(a,b);
+    // int sum(int a,int b)  // int a ---> 3, int b -----> 5
 
-    cout<<c;
+    cout<<c<<endl;
+    cout<<a<<" "<<b<<endl;   // these values of a,b are after I call the function "sum"
 
     return 0;
+}
+
+int sum(int a, int b)
+{
+    return a+b;
 }
