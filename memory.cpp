3509f158a3665920fd496314c46c933d42075a71
abdullah <3703abdullahashraf@gmail.com>
2023-07-11 16:08:10 +0530

    Lecture-11, Part-2

--- memory.cpp
+++ memory.cpp
@@ -5,8 +5,10 @@ int main()
 {
     long long int a;
     int b;
+    float c;
 
-    cout<<"The size of int is :"<<sizeof(b)<<" The size of long long int is:"<<sizeof(a);
+    //cout<<"The size of int is :"<<sizeof(b)<<" The size of long long int is:"<<sizeof(a);
+    cout<<"The address of c is "<<&c<<endl<<"The address of b is "<<b;
 
     return 0; 
 }
