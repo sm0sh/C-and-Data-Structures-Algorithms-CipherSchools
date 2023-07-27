c185117e3d2d6138c57c4cd705b56363ab499497
abdullah <3703abdullahashraf@gmail.com>
2023-07-27 20:42:37 +0530

    Lecture-34, Part-1

---
+++ tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// This is how the nodes of a tree are defined
+
+class Node
+{
+    public:
+    int data;
+    Node * left;
+    Node * right;
+
+    // Note : since each node has the address to the node corresponding to the left and right of the node we are able to access the whole tree through the root node
+};
+
+int main()
+{
+    string CEO;
+
+    string PA,CFO;
+
+
+}
+
