42548cdd8e88e623f6b91875d5ab670d0868ead3
abdullah <3703abdullahashraf@gmail.com>
2023-07-27 22:37:10 +0530

    Lecture-34, Part-3

--- tree.cpp
+++ tree.cpp
@@ -20,38 +20,86 @@ class Node
     }
 };
 
-void buildTree(Node * root)
+void buildTree(Node * & root)
 {
     // This is the start of the creation of a binary tree
     
     int var;
 
-    cout<<"Enter the value : ";
+    //cout<<"Enter the value : ";
     cin>>var;
 
     // If the user inputs -1, that means there is no node present over there
 
-    if(var==-1) return;
+    if(var == -1) return;
 
     root = new Node(var);
 
-    cout<<"Now enter the data which you want to be the left of "<<var<<" ";
+    //cout<<"Now enter the data which you want to be the left of "<<var<<" ";
     buildTree(root->left);
-    cout<<"Now enter the data which you want to be the right of "<<var<<" ";
+    //cout<<"Now enter the data which you want to be the right of "<<var<<" ";
     buildTree(root->right);
 }
 
 // First we will look at pre-order transversal
-
 // pre-order transversal implies that we first transverse the node, then the elements to the left of it and then the elements to the right of it
 
+void printPreordertransversal(Node * & root)
+{
+    // First we print N,L,R
+
+    if(root==NULL)
+    {
+        return;
+    }
+
+    cout<<root->data<<" ";
+    printPreordertransversal(root->left);
+    printPreordertransversal(root->right);
+}
+
+void printInordertransversal(Node * & root)
+{
+    // first we print L,N,R
+
+    if(root == NULL) return;
+
+    printInordertransversal(root->left);
+    cout<<root->data<<" ";
+    printInordertransversal(root->right);
+
+}
+
+void printPostordertransversal(Node * & root)
+{
+    // first we print L,R,N
+
+    if(root == NULL) return;
+
+    printPostordertransversal(root->left);
+    printPostordertransversal(root->right);
+    cout<<root->data<<" ";
+
+}
 int main()
 {
     // Now since we have created a node, let us start with the creation of a binary tree
     
-    Node * root;
+    Node * root=NULL;
 
     buildTree(root);
+
+    //5 9 1 -1 -1 7 -1 -1 3 2 -1 -1 5 9 -1 -1 10 -1 -1
+
+    cout<<"The elements transversed in preorder transversal is "<<endl;
+    printPreordertransversal(root);
+
+    cout<<endl<<"The elements transversed in Ineorder transversal is "<<endl;
+    printInordertransversal(root);
+
+    cout<<endl<<"The elements transversed in postorder transversal is "<<endl;
+    printPostordertransversal(root);
+
 
     return 0;
 
